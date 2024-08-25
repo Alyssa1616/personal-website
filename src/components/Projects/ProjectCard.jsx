e3efc3c06ddea033893a67a4a9650f37d5754abc
alyssaf16 <alyssa_feinberg@brown.edu>
2024-08-25 05:03:00 -0400

    cleaned up projects spacing

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -6,14 +6,16 @@ const ProjectCard = ({ title, description, imageUrl, webLink, githubLink }) => {
       <img src={imageUrl} alt={title} className="project-image" />
       <h3>{title}</h3>
       <p>{description}</p>
+      <div className="d-flex justify-content-center button-container">
       {webLink ?
-      <a href={webLink} target="_blank" rel="noopener noreferrer" className="github-link">
+      <a href={webLink} className="btn github-link mx-2" target="_blank" rel="noopener noreferrer">
         View Website
       </a> : null}
       {githubLink ?
-      <a href={githubLink} target="_blank" rel="noopener noreferrer" className="github-link">
+      <a href={githubLink} className="btn github-link mx-2" target="_blank" rel="noopener noreferrer">
         View on GitHub
       </a> : null}
+      </div>
     </div>
   );
 };
